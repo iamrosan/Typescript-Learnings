78d1cf5a58b5feb3f2471ba70ad2f5152ad9c6a2
iamrosan <roshanshrestha21.63@gmail.com>
2023-11-11 22:03:46 +0545

    feat: mui text field component added

--- todo-React-js/TODO/src/components/toDoComponent/ToDoComponent.jsx
+++ todo-React-js/TODO/src/components/toDoComponent/ToDoComponent.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ButtonComponent from "../shared/ButtonComponent";
 import FileHeadingComponent from "../shared/FileHeadingComponent";
+import TextFieldComponent from "../shared/TextFieldComponent";
 
 const ToDoComponent = () => {
   return (
     <>
       <FileHeadingComponent title={"TODO"} variant={"h5"} />
+      <TextFieldComponent label={"Todo"} />
       <ButtonComponent
         variant={"contained"}
         label={"Click me"}
