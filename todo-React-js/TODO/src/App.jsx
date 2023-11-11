8cd64cb5bf640df199869e87c9444db1edaab2a5
iamrosan <roshanshrestha21.63@gmail.com>
2023-11-11 20:34:04 +0545

    feat: file structure defined

---
+++ todo-React-js/TODO/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import ToDoComponent from "./components/toDoComponent/toDoComponent";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <ToDoComponent />
+    </>
+  );
+}
+
+export default App;
